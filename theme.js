8e87775ed7c2e2c2c74027e35a9675c0f66e4287
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-07 19:52:54 +0900

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,3 @@
+if(document.cookie.replace(/(?:(?:^|.*;\s*)theme\s*\=\s*([^;]*).*$)|^.*$/, "$1") === "dark") {
+    document.getElementById("theme").setAttribute("href", "https://adtemp.github.io/style-dark.css");
+}
