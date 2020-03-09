5d11216b735fce0efad604af4b1c6aec8026b268
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-09 09:29:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ tt.onclick = () => {
     if(tt.getAttribute("switch") === "on") {
         tm.setAttribute("href", "https://adtemp.github.io/style.css");
         tt.setAttribute("switch", "off");
-        document.cookie = "theme=light;domain=test87.hatenablog.com";
+        document.cookie = "theme=light;domain=hatenablog.com";
     } else {
         tm.setAttribute("href", "https://adtemp.github.io/style-dark.css");
         tt.setAttribute("switch", "on");
-        document.cookie = "theme=dark;domain=test87.hatenablog.com";
+        document.cookie = "theme=dark;domain=hatenablog.com";
     }
 };
 
