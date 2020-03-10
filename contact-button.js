f212c37d231111268fa33a8fc99e9c901b97c9c9
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-11 08:56:10 +0900

    Update contact-button.js

--- contact-button.js
+++ contact-button.js
@@ -1,4 +1,5 @@
 document.getElementById("submit-button").onclick = () => {
     document.myform.submit();
     document.querySelector(".content-main").style.display = "none";
+    document.getElementById('thxMessage').style.display = "block";
 };
